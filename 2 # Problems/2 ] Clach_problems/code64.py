3684300f48436bc6d612d654a435c31d03b95e72
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-19 18:52:11 +0300

    one adtional problems

---
+++ 2 # Problems/2 ] Clach_problems/code64.py
@@ -0,0 +1,17 @@
+#  -- Hash Clash --
+# s = ' '.join(c for c in s.lower() if c.isalpha())
+# if s[::-1]==s:print("");exit()
+# from itertools import cycle
+# C = cycle([10,6,0,10,6,24])
+# while 1: print(next(C))
+# print(chr(48))  # convert from Asscicode into char
+# from collections import defaultdict
+# # Create a defaultdict with a default value of 0
+# dic = defaultdict(int)
+# print(sum(ord(i)if ord(i)%2==1else 0 for i in input()))
+# c = eval(f"{c}{a}{b}")
+s = input()
+ss = ''
+for i in range(1,10):
+    if s.find(str(i)) == -1:
+       print(i)
