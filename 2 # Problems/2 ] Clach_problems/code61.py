8a67e8a8114a9622bf1007c94dfb853ae998bebd
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-19 18:45:43 +0300

    All files

---
+++ 2 # Problems/2 ] Clach_problems/code61.py
@@ -0,0 +1,20 @@
+#  -- Hash Clash --
+# s = ' '.join(c for c in s.lower() if c.isalpha())
+# if s[::-1]==s:print("");exit()
+# from itertools import cycle
+# C = cycle([10,6,0,10,6,24])
+# while 1: print(next(C))
+# print(chr(48))  # convert from Asscicode into char
+# from collections import defaultdict
+# # Create a defaultdict with a default value of 0
+# dic = defaultdict(int)
+# print(sum(ord(i)if ord(i)%2==1else 0 for i in input()))
+# c = eval(f"{c}{a}{b}")
+import sys
+import math
+
+s1 = int(input())
+s2 = int(input())
+s3 = int(input())
+s4 = int(input())
+print(max({(s1+s2), (s2+s3), (s3+s4)}))
