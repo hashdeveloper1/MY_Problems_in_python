1ec5cf0969b8ebed3119a8a7f60efcb27e790ac8
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-21 14:36:20 +0300

    tow adtional problems

--- 1 # Practices/1 # Basics/7  ] Sets/Sets_29.py
+++ 1 # Practices/1 # Basics/7  ] Sets/Sets_29.py
@@ -22,49 +22,31 @@
 # s1.intersection_update(s2)
 # print(s1.symmetric_difference(s2))
 # s1.symmetric_difference_update(s2)
+# print(a.issuperset(b))  # True
+# print(a.issubset(b))  # False
+# print(a.isdisjoint(b))  # False
 # ===============================
 print("="*10)
-s1 = {1, 2, 3}
-s2 = {1, 2}
-print(s1)
-print(s1.difference(s2))
-print(s1)
-print("="*10)
-# ===============================
-s1 = {1, 2, 3}
-s2 = {1, 2}
-print(s1)
-s1.difference_update(s2)
-print(s1)
+a = {1, 2, 3}
+b = {1, 2}
+c = {1, 2, 3, 4, 5}
+print(a.issuperset(b))  # True
+print(a.issuperset(c))  # False
 print("="*10)
 # ===============================
 print("="*10)
-s1 = {1, 2, 3}
-s2 = {1, 2}
-print(s1)
-print(s1.intersection(s2))
-print(s1)
-print("="*10)
-# ===============================
-s1 = {1, 2, 3}
-s2 = {1, 2}
-print(s1)
-s1.intersection_update(s2)
-print(s1)
+a = {1, 2, 3}
+b = {1, 2}
+c = {1, 2, 3, 4, 5}
+print(a.issubset(b))  # False
+print(a.issubset(c))  # True
 print("="*10)
 # ===============================
 print("="*10)
-s1 = {1, 2, 3}
-s2 = {1, 2, 4}
-print(s1)
-print(s1.symmetric_difference(s2))
-print(s1)
-print("="*10)
-# ===============================
-s1 = {1, 2, 3}
-s2 = {1, 2, 4}
-print(s1)
-s1.symmetric_difference_update(s2)
-print(s1)
+a = {1, 2, 3}
+b = {11, 22}
+c = {1, 2, 3, 4, 5}
+print(a.isdisjoint(b))  # False
+print(a.isdisjoint(c))  # True
 print("="*10)
 # ===============================
---
+++ 2 # Problems/2 ] Clach_problems/code72.py
@@ -0,0 +1,16 @@
+#  -- Hash Clash --
+# s = ' '.join(c for c in s.lower() if c.isalpha())
+# if s[::-1]==s:print("");exit()
+# from itertools import cycle
+# C = cycle([10,6,0,10,6,24])
+# while 1: print(next(C))
+# print(chr(48))  # convert from Asscicode into char
+# from collections import defaultdict
+# # Create a defaultdict with a default value of 0
+# dic = defaultdict(int)
+# print(sm(ord(i)if ord(i)%2==1else 0 for i in input()))
+# c = eval(f"{c}{a}{b}")
+import sys
+import math
+n = int(input())
+print(math.factorial(n))
