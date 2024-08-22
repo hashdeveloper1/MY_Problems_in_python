c6f4fec0e0050a39be2927902fe87fc18ab160f6
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-22 13:54:28 +0300

    tow adtional problems

--- 1 # Practices/1 # Basics/8  ] Dictionary/Dictionary_31.py
+++ 1 # Practices/1 # Basics/8  ] Dictionary/Dictionary_31.py
@@ -1,65 +1,54 @@
 # ===============================
-#        -- Dictionary --
+#     -- Dictionary Methods --
+# ===============================
+# dic.clear()
+# dic.update()
+# dic.copy()
+# dic.keys()
+# dic.values()
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+print(dic)
+dic.clear()
+print(dic)
+print("="*10)
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+print(dic)
+dic.update({'salary': 50000})
+print(dic)
+dic['Gender'] = 'Male'
+print(dic)
+print("="*10)
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+b = dic.copy()
+print(b)
+print(dic)
+dic['skills'] = ['HTML', 'CSS', 'JS', 'Python', 'C++', 'XML', 'Odoo', 'OOP', 'Data structure']
+print(dic)
+print(b)
+print("="*10)
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+print(dic.keys())
+print(dic.values())
+print("="*10)
 # ===============================
 
-# ===============================
-print("="*10)
-user = {
-    'name': 'Hashem',
-    'age': 24,
-    'salary': 24000,
-    'skills': ['HTML', 'CSS', 'JS']
-}
-print(user)
-print(user['name'])
-print(user['age'])
-print(user['skills'][2])
-print(user.get('salary'))
-print(user.keys())
-print(user.values())
-print("="*10)
-# ===============================
-print("="*10)
-languages = {
-    'one': {
-        'name': 'python',
-        'progress': '70%',
-    },
-    'tow': {
-        'name': 'Odoo',
-        'progress': '60%',
-    },
-    'three': {
-        'name': 'postgresql',
-        'progress': '20%',
-    }
-}
-print(languages)
-print(languages['one'])
-print(languages['tow'])
-print(languages['three'])
-print("="*10)
-# ===============================
-print("="*10)
-
-framwork1 = {
-    'name': 'python',
-    'progress': '70%',
-}
-framwork2 = {
-    'name': 'Odoo',
-    'progress': '60%',
-}
-framwork3 = {
-    'name': 'postgresql',
-    'progress': '20%',
-}
-
-Allframwork = {
-    'one': framwork1,
-    'tow': framwork2,
-    'three': framwork3,
-}
-print(Allframwork)
-print("="*10)
-# ===============================
