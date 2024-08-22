914befc2641284541236a25a6001750f90bd3e43
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-22 14:44:34 +0300

    Practices to this Day

---
+++ 1 # Practices/1 # Basics/9  ] Boolean Values/Boolean_33.py
@@ -0,0 +1,27 @@
+# ===============================
+#          -- Boolean --
+# ===============================
+s = ""
+print(s.isspace())
+s = " "
+print(s.isspace())
+print('*'*50)
+print(10 > 20)
+print(10 == 20)
+print(10 < 20)
+print('*'*50)
+print(bool(10))
+print(bool([1, 2, 3]))
+print(bool(True))
+print(bool(100))
+print(bool(10.2))
+print('*'*50)
+print(bool(0))
+print(bool(False))
+print(bool([]))
+print(bool({}))
+print(bool(()))
+print(bool(None))
+print(bool(''))
+print(bool(""))
+print('*'*50)
---
+++ 1 # Practices/1 # Basics/9  ] Boolean Values/Boolean_34.py
@@ -0,0 +1,15 @@
+# ===============================
+#          -- Boolean --
+# ===============================
+# print(s.isspace())
+# print(bool())
+# and
+# or
+# not
+# ===============================
+a = 1
+b = 2
+c = 3
+print(a < c and b < c and a + b == c)
+print((not a > c) and b < c and a + b == c)
+print((not a > c) or b < c or a + b == c)
