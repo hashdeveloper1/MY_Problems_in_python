9f111041d87af3c05b095f01088770197ba04424
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-22 15:32:58 +0300

    Practices to this Day

---
+++ 1 # Practices/1 # Basics/8  ] Dictionary/Dictionary_31.py
@@ -0,0 +1,43 @@
+# ===============================
+#     -- Dictionary Methods --
+# ===============================
+# dic.clear()
+# dic.update()
+# dic.copy()
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+print(dic)
+dic.clear()
+print(dic)
+print("="*10)
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+print(dic)
+dic.update({'salary': 50000})
+print(dic)
+dic['Gender'] = 'Male'
+print(dic)
+print("="*10)
+# ===============================
+print("="*10)
+dic = {
+    'name': 'hashem',
+    'age': 24
+}
+b = dic.copy()
+print(b)
+print(dic)
+dic['skills'] = ['HTML', 'CSS', 'JS', 'Python', 'C++', 'XML', 'Odoo', 'OOP', 'Data structure']
+print(dic)
+print(b)
+print("="*10)
+# ===============================
+
