a9218393309bcef355818b587f58ac83152df223
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-28 20:30:35 +0300

    All files

--- 1 # Practices/1 # Basics/15 ] Control_Flow/Control_Flow_42.py
+++ 1 # Practices/1 # Basics/15 ] Control_Flow/Control_Flow_42.py
@@ -1,14 +1,15 @@
 name = "Hashem"
-country = "Egypt"
+country = "ASW"
+student = True
 course_name = "Python Course"
 course_price = 100
 
-if country == 'Egypt':
+if country == 'Egypt' or country == 'ASW':
     print(f'Hello {name} ....')
-    print(f"Because you from {country} Your course Price is : ${course_price-80}")
-elif country == 'ASW':
-    print(f'Hello {name} ....')
-    print(f"Because you from {country} Your course Price is : ${course_price-60}")
+    if student:
+        print(f"Because you from {country} Your course Price is : ${course_price-90}")
+    else:
+        print(f"Because you from {country} Your course Price is : ${course_price - 80}")
 else:
     print(f'Hello {name} ....')
     print(f"Because you from {country} Your course Price is : ${course_price - 30}")
