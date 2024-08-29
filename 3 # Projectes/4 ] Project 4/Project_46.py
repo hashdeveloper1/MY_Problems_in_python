40c681126a4b7015d2011ff8e8aca17aa671f3ec
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-08-29 15:12:11 +0300

    upload new Lesson 46 is project admin system in Course

--- 3 # Projectes/4 ] Project 4/Project_46.py
+++ 3 # Projectes/4 ] Project 4/Project_46.py
@@ -1,41 +1,45 @@
 #######################################################
-#         -- Calculate Age Project version 2 --       #
-#         -- BY Hashem Ahmed Mahmoud ELMahdi --       #
+#    -- Admin System Login as Admin --                #
+#    -- BY Hashem Ahmed Mahmoud EL Mahdi --           #
 #######################################################
-# write note like good
+# Message to Start Program
 print('#'*40)
-print(' [ You Can to Enter W or M or D ] '.center(40, '#'))
+print(' [ Start Program ] '.center(40, '#'))
 print('#'*40)
-# Collect age by input
-age = int(input("Enter Your Age : ").strip())
-# Collect unit by input
-unit = input("Enter Your Unit Age { Months , Weeks , Days }: ").strip().lower()
-# =========================
-# Collect Months And Days And Weeks And More As Operation Statements
-month = age * 12
-week = month * 4
-day = week * 7
-hours = day * 24
-mints = hours * 60
-seconds = mints * 60
-# =========================
-# Check Choice And Implement This Choice By Return Result
-if unit == 'months' or unit == 'm':
-    print("You Choose Months")
-    print(f"You Lived {month:,} Months")
-elif unit == 'weeks' or unit == 'w':
-    print("You Choose Weeks")
-    print(f"You Lived {week:,} Weeks")
-elif unit == 'Days' or unit == 'd':
-    print("You Choose Days")
-    print(f"You Lived {day:,} Days")
+# Admin List
+admins = ['Hashem', 'Ali', 'Abdo']
+# Take Username By Input Method
+name = input("Enter Your Name : ").strip().capitalize()
+
+# If Name is in Admins
+if name in admins:
+    print(f"Hello {name} Welcome Back")
+    option = input('Delete or Update | [D,U] Your Name : ').strip().capitalize()
+    # Update Option
+    if option == 'Update' or option == 'U':
+        new_name = input('Please Enter New Name : ').strip().capitalize()
+        admins[admins.index(name)] = new_name
+        print("Your Name updated")
+        print(admins)
+    # Delete Option
+    elif option == 'Delete' or option == 'D':
+        admins.remove(name)
+        print("Your Name Deleted")
+        print(admins)
+    # wrong Option
+    else:
+        print("Please Enter Valid Option")
+# If Name is not found in Admins
 else:
-    print("You Lived for")
-    print(f"{month} Month")
-    print(f"{week:,} Week")
-    print(f"{day:,} Days")
-    print(f"{mints:,} Mints")
-# =========================
+    # To Add or not
+    status = input("You Is not Admin , To Add You Enter [ Y or N ]: ").strip().capitalize()
+    if status == 'Yes' or status == 'Y':
+        print("You is been added")
+        admins.append(name)
+        print(admins)
+    else:
+        print("You Are Not Added.")
+
 # This Design To End Program
 print('#'*40)
 print(' [ End Program ] '.center(40, '#'))
